22139b98c23b9fb19805e4f1e8ba3ff55658996e
lil-fox <qute.lil.fox@gmail.com>
2025-03-12 21:54:06 +0100

    init

---
+++ src/main/java/net/lilfox/liltweaks/LilTweaks.java
@@ -0,0 +1,10 @@
+package net.lilfox.liltweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LilTweaks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/lilfox/liltweaks/client/LilTweaksClient.java
@@ -0,0 +1,11 @@
+package net.lilfox.liltweaks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LilTweaksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
